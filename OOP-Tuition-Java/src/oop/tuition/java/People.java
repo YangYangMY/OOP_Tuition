2eacc624956de7fd29e9dd333689020e9367906f
wilsontcm69 <86353208+wilsontcm69@users.noreply.github.com>
2022-03-18 00:29:16 +0800

    Add files via upload

---
+++ OOP-Tuition-Java/src/oop/tuition/java/People.java
@@ -0,0 +1,16 @@
+class People {
+    String name;
+    int age;
+    char sex;
+    String phoneNum;
+    String email;
+
+    public People() {
+        this.name = null;
+        this.age = 0;
+        this.sex = 0;
+        this.phoneNum = null;
+        this.email = null;
+    }   
+    
+}
